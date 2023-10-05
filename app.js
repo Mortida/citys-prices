2f4e2abeaeee64299d1f596c1dac77f6d6501124
Murtada <43283994+Mortida@users.noreply.github.com>
2023-10-05 23:03:52 +0000

    General changes

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require("express");
-const db = require("./db");
-const cityRoutes = require("./cityController");
 const cors = require("cors");
+const path = require("path");
+const connectDB = require("./db");
+const router = require("./Router");
+
 require("dotenv").config();
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(cors()); // Enable CORS for all routes
-app.use(cityRoutes);
+app.use(cors());
+app.use(router);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB(); // Wait for the database connection to be established
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+    process.exit(1); // Exit the process if there's a database connection error
+  }
+};
+
+startServer();
--- db.js
+++ db.js
@@ -1,12 +1,24 @@
 const mongoose = require("mongoose");
 
-const mongoUrl = "mongodb+srv://test:09876Poiuy@testcluster.ozbappd.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoUrl =
+  "mongodb+srv://test:09876Poiuy@testcluster.ozbappd.mongodb.net/?retryWrites=true&w=majority";
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
+const connectDB = async () => {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = mongoose.connection;
+    db.on("error", (error) => {
+      console.error("MongoDB connection error:", error);
+      reject(error); // Reject the promise if there's an error
+    });
+    db.once("open", () => {
+      console.log("Connected to MongoDB");
+      resolve(); // Resolve the promise when the connection is open
+    });
+  });
+};
 
-module.exports = db;
+module.exports = connectDB;
