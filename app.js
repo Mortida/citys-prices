99ae7be60771b35255156b1d6ed4654361474885
Murtada <wadosmangprail@gmail.com>
2023-10-05 18:03:56 +0100

    Change on env

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const db = require("./db");
 const cityRoutes = require("./cityController");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
 
 app.use(express.json());
 app.use(cors()); // Enable CORS for all routes
 app.use(cityRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
