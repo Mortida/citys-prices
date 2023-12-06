db63b5e6094688c81908eff7e8c6853527ac6837
Murtada <43283994+Mortida@users.noreply.github.com>
2023-12-06 20:35:11 +0000

    Update cityModel.js

--- cityModel.js
+++ cityModel.js
@@ -1,10 +1,25 @@
 const mongoose = require("mongoose");
 
+// Schema for the City model
 const citySchema = new mongoose.Schema({
   name: { type: String, unique: true },
   price: Number,
 });
 
+// Schema for the Header model (assuming a different schema)
+const headerSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+});
+
+// Model for the City collection
 const City = mongoose.model("City", citySchema);
 
-module.exports = City;
+// Model for the Header collection
+const Header = mongoose.model("Header", headerSchema);
+
+// Export both models
+module.exports = {
+  City: City,
+  Header: Header,
+};
